25924f64426063d19405fc2fdc8eeeffde010d29
Ankit-29 <iamankitkumar29@gmail.com>
2020-08-26 20:49:55 +0530

    Algo: Jump Search

---
+++ Algorithms/Search/jumpSearch.py
@@ -0,0 +1,39 @@
+'''
+    Algo: Binary Search
+    Type: Searching
+
+    Time-Complexity: 
+        - Best: O(1)
+        - Worst: O(√n)
+    Space-Complexity: O(1)
+'''
+import math
+from typing import List
+
+
+def jumpSearch(arr: List[int], ele: int) -> int:
+    size = len(arr)
+    jumpBlock = int(math.sqrt(size))
+    step = jumpBlock
+    prev = 0
+    while(arr[min(step, size)-1] < ele):
+        prev = step
+        step += jumpBlock
+        if(prev >= size):
+            return -1
+
+    while(arr[prev] < ele):
+        prev += 1
+        if(prev == min(step, size)):
+            return -1
+
+    if(arr[prev] == ele):
+        return prev
+
+    return -1
+
+
+arr = [2, 3, 4, 10, 40]
+ele = 10
+# 3
+print(jumpSearch(arr, ele))
